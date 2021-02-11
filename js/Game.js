8eb022825e622bedca4f0ab4650af5e5f2bc66df
sjpriya <72385035+sjpriya@users.noreply.github.com>
2021-02-11 14:46:28 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -5,7 +5,7 @@ class Game {
     var gameStateRef  = database.ref('gameState');
     gameStateRef.on("value",function(data){
        gameState = data.val();
-    })
+    });
 
   }
 
@@ -15,21 +15,10 @@ class Game {
     });
   }
 
-  async start(){
-    if(gameState === 0){
-      player = new Player();
-      var playerCountRef = await database.ref('playerCount').once("value");
-      if(playerCountRef.exists()){
-        playerCount = playerCountRef.val();
-        player.getCount();
-      }
-      form = new Form()
-      form.display();
-    }
-  }
+  
 
   
 
     
-  }
+  
 }
